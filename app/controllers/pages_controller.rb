0f5d3e3294cf33112fc7f6122d915f531c4f389c
Rui Cheng <ktei2008@gmail.com>
2013-08-17 19:35:44 +1000

    Infrastructure

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def home
+    @use_container = false
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moment::Application.config.secret_token = '1c382ec5925e3ab46c2084c575205eea991d101973bfe9ae5916bf6c83a630599b6376c755379de3896d57d5ec921ad0d4db3ceb8140662dec9f5be744af5a7e'
